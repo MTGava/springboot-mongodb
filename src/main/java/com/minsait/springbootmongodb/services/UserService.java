daa7498aef03c542e68ee87bf1bf8294b9a1a6b5
Matheus Gava <mgava@minsait.com>
2021-10-23 19:14:56 -0300

    Obtendo um usuário por id

--- src/main/java/com/minsait/springbootmongodb/services/UserService.java
+++ src/main/java/com/minsait/springbootmongodb/services/UserService.java
@@ -1,20 +1,27 @@
 package com.minsait.springbootmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.minsait.springbootmongodb.domain.User;
 import com.minsait.springbootmongodb.repository.UserRepository;
+import com.minsait.springbootmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
