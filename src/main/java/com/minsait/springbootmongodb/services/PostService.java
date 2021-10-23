11a5f99b0b1065072b7a24f7fa277ac1de31dc89
Matheus Gava <mgava@minsait.com>
2021-10-23 20:10:55 -0300

    Obtendo um post por id

---
+++ src/main/java/com/minsait/springbootmongodb/services/PostService.java
@@ -0,0 +1,22 @@
+package com.minsait.springbootmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.minsait.springbootmongodb.domain.Post;
+import com.minsait.springbootmongodb.repository.PostRepository;
+import com.minsait.springbootmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
